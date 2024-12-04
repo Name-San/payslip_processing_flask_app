f69b8ff50f49c9ea9795e519cbfa49e7a7b6313d
Name-San <eman.jandog@gmail.com>
2024-12-04 23:09:56 +0800

    added

--- app.py
+++ app.py
@@ -129,5 +129,5 @@ def oauth2callback():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Use Render's PORT or default to 5000
-    app.run(port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
     
